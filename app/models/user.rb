16a90a888fab43294041fb14d4a7f2fb8de0a2c8
Ismail Hossain Fahim <ifahim92@gmail.com>
2025-05-25 14:34:51 +0600

    Add one-to-many association between users and articles rom backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles
     validates :usersname, presence: true, 
                             uniqueness: { case_sensitive: false }, 
                             length: {minimum: 3, maximum: 25}
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_17_123210) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_25_065539) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
