084ba3e09ec1aeee86c0b69220f8447248d08046
Ismail Hossain Fahim <ifahim92@gmail.com>
2025-06-02 13:30:48 +0600

    users-edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,11 @@ class UsersController < ApplicationController
         end
     end
     def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+    def index
+        @users = User.all
     end
     def edit
         @user = User.find(params[:id])
@@ -20,7 +25,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was successgully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
